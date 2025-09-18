2fd49861aa670dbaf93578b0daf63af07439a524
Amaan Akhter <amaanakhter19@gmail.com>
2025-09-18 23:17:25 +0530

    models-created

---
+++ models/product-model.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongosse');
+
+const productSchema=mongoose.productSchema({
+    image:String,
+    name:String,
+    price:Number,
+    discount:{
+        type:Number,
+        default:0,
+    },
+    bgcolor:String,
+    panelcolor:String,
+    textcolor:String,
+})
+module.exports=mongoose.model('product',productSchema);
